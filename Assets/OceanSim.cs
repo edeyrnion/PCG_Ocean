6e27408a476dcc450c397ccbeef311e9ff8c2c03
edeyrnion <matthias.treubrodt@gmail.com>
2018-08-27 14:21:51 +0200

    blub

---
+++ Assets/OceanSim.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class OceanSim : MonoBehaviour
+{
+    [SerializeField] private int _gridSize = 100;
+    [SerializeField] private float _cellSize = 10;
+    [SerializeField] private PerlinNoiseLayer _perlinNoiseLayer;
+
+    private Mesh _mesh;
+
+    private void Awake()
+    {
+        _mesh = new PlaneGenerator().Generate(_gridSize, _cellSize);
+        _mesh.MarkDynamic();
+
+        GetComponent<MeshFilter>().mesh = _mesh;
+    }
+
+    private void Update()
+    {
+        var vertices = _mesh.vertices;
+
+        Vector3[] vertices = new Vector3[_mesh.vertices.Length];
+
+        var jobHandle = new JobHandle();
+        var vertexArray = new NativeArray<Vector3>(vertices, Allocator.TempJob);
+
+        var Job = new SetPerlinNoiseJob
+        {
+            Vertices = vertexArray,
+            Layer = _perlinNoiseLayer,
+            Time = Time.timeSinceLevelLoad
+        };
+
+        jobHandle = Job.Schedule(vertexArray.Length, 250);
+
+        jobHandle.Complete();
+
+        vertexArray.CopyTo(vertices);
+        vertexArray.Dispose();
+
+        _mesh.vertices = vertices;
+
+        _mesh.RecalculateNormals();
+    }
+}
--- Assets/PerlinNoiseJob.cs
+++ Assets/PerlinNoiseJob.cs
@@ -2,6 +2,7 @@
 using Unity.Jobs;
 using UnityEngine;
 
+
 public struct AddPerlinNoiseJob : IJobParallelFor
 {
     public NativeArray<Vector3> Vertices;
@@ -16,6 +17,25 @@ public struct AddPerlinNoiseJob : IJobParallelFor
         var z = vertex.z * Layer.Scale + Time * Layer.Speed;
 
         vertex.y += (Mathf.PerlinNoise(x, z) - 0.5f) * Layer.Height;
+
+        Vertices[i] = vertex;
+    }
+}
+
+public struct SetPerlinNoiseJob : IJobParallelFor
+{
+    public NativeArray<Vector3> Vertices;
+    public PerlinNoiseLayer Layer;
+    public float Time;
+
+    public void Execute(int i)
+    {
+        var vertex = Vertices[i];
+
+        var x = vertex.x * Layer.Scale + Time * Layer.Speed;
+        var z = vertex.z * Layer.Scale + Time * Layer.Speed;
+
+        vertex.y = (Mathf.PerlinNoise(x, z) - 0.5f) * Layer.Height;
 
         Vertices[i] = vertex;
     }
