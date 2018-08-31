c22d17e2d8b8a22867b65dda1aeb677085c791da
edeyrnion <matthias.treubrodt@gmail.com>
2018-08-31 12:23:13 +0200

    More stuff

--- Assets/Scripts/OceanSimulation.cs
+++ Assets/Scripts/OceanSimulation.cs
@@ -9,28 +9,32 @@ public class OceanSimulation : MonoBehaviour
     [SerializeField] private int _gridSize = 10;
     [SerializeField] private float _cellSize = 1;
     [SerializeField] private List<PerlinNoiseLayer> _perlinNoiseLayers;
+    [SerializeField] private Material _material;
 
     private Mesh _mesh;
+    private Vector3[] _vertices;
 
     private void Awake()
     {
-        _mesh = PlaneGenerator.Generate(_gridSize, _cellSize);
+        _mesh = PlaneGenerator.Generate(_gridSize, _cellSize, "OceanMesh");
         _mesh.MarkDynamic();
 
-        GetComponent<MeshFilter>().mesh = _mesh;
+        _vertices = _mesh.vertices;
     }
 
     private void Update()
     {
-        var vertices = _mesh.vertices;
+        _vertices = _mesh.vertices;
 
-        FlattenVertices(vertices);
+        FlattenVertices(_vertices);
 
-        ExecutePerlinNoiseJobs(vertices);
+        ExecutePerlinNoiseJobs(_vertices);
 
-        _mesh.vertices = vertices;
+        _mesh.vertices = _vertices;
         //_mesh.SetVertices(vertices.ToList());
         _mesh.RecalculateNormals();
+
+        Graphics.DrawMesh(_mesh, Vector3.zero, Quaternion.identity, _material, 0);
     }
 
     private void FlattenVertices(Vector3[] vertices)
